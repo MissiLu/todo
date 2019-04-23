19499f758aa7a359d0dca062dd01ca5207aab8c3
MissiLu <49556736+MissiLu@users.noreply.github.com>
2019-04-23 15:09:17 +0300

    Add files via upload

--- todo.js
+++ todo.js
@@ -1,5 +1,20 @@
 function addToList() {
-    var todoItem = "<li>" + document.getElementById("myText").value +"</li>"
+    var todoItemInput = document.getElementById("myText").value
+    console.log(typeof todoItemInput)
+    if (todoItemInput == "") { 
+        return
+    }
+    // debugger //точка остановки
+    var todoItem = '<li><input type="checkbox"/>' + todoItemInput + '<button type="button">delete</button></li>'
+    console.log(todoItem)
     document.getElementById("myText").value = ""
-  document.getElementById("list").innerHTML += todoItem;
+    
+    document.getElementById("list").innerHTML += todoItem;
+    // var string = '' // ""
+    // var int = 123
+    // var float = 1.23
+    // var object = {attr1: 123}
+    // var array = [string, int, float]
+    // var nil = null
+    // var boolean = true //false
 }
